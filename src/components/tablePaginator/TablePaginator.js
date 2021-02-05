390e8964f8657824bca7b814016c55493baabaf8
Marko Dudic <markonijum@gmail.com>
2021-02-05 20:28:04 +0100

    Adding pagination

--- src/components/tablePaginator/TablePaginator.js
+++ src/components/tablePaginator/TablePaginator.js
@@ -1,51 +1,100 @@
 import React, {useEffect} from 'react';
+import styled from "styled-components"
 import { useSelector, useDispatch } from "react-redux";
-import { Link, useLocation  } from "@reach/router"
-
+import { Link  } from "@reach/router"
+import { GrPrevious, GrNext } from "react-icons/gr";
 import {
     selectTotalPages,
     selectOffset,
     setTotalPages,
-    selectCurrentPage
+    selectPaginationList,
+    selectCurrentPage,
+    selectNeighbours,
+    setCurrentPage
 } from "./tablePaginatorSlice";
 import {
     selectSortedFilteredList,
     selectSortedFilteredPaginatedList
 } from "../tableList/tableListSlice";
 
-function TablePaginator(props) {
+function TablePaginatorRaw({className}) {
     const dispatch = useDispatch()
     const currentPage = useSelector(selectCurrentPage)
     const totalPages = useSelector(selectTotalPages)
+    const neighbours = useSelector(selectNeighbours)
+    const paginationList = useSelector(selectPaginationList)
     const offset = useSelector(selectOffset)
     const list = useSelector(selectSortedFilteredList)
     const paginatedList = useSelector(selectSortedFilteredPaginatedList)
 
+    console.log("PAGINATION LIST: ", paginationList)
+
     useEffect(() => {
-        dispatch(setTotalPages(list.length/offset))
+        dispatch(setTotalPages(Math.ceil(list.length/offset)))
     },[ offset , list,  dispatch ])
 
-    const renderPagination = () => {
-
-        const rows = []
-        for(let page = 0 ; page < totalPages ; page++) {
-            rows.push(<li key={page + 1} className="page-item"><Link className="page-link" to={`?page=${page + 1}&offset=${offset}`}>{page + 1}</Link></li>)
-        }
-        return rows
-    }
     return (
-        <div style={{marginTop: "50px"}} className="row">
-            {paginatedList.length === 0 ? null : <div className="col">
+        <div style={{marginTop: "50px"}} className={`${className} row`}>
+            {paginatedList.length === 0 ? null : <div className="col nav-wrapper">
                 <nav aria-label="Page navigation example">
-                    <ul className="pagination">
-                        <li className="page-item"><Link className="page-link" to={ `?page=${currentPage - 1 }&offset=${offset}` }>Previous</Link></li>
-                        { renderPagination() }
-                        <li className="page-item"><Link className="page-link" to={ `?page=${currentPage + 1 }&offset=${offset}` }>Next</Link></li>
-                    </ul>
+
+                    { paginationList.length > 0 &&  (<ul className="pagination">
+
+                        { paginationList.map((page, index) => {
+
+                            if(page === "PREV") {
+                                return <li key={index} className="page-item"><Link className="page-link" to={`?page=${currentPage - (neighbours*2) - 1}&offset=${offset}`} href="#" >&#8678;</Link></li>
+                            }
+                            if(page === "NEXT") {
+                                return <li key={index} className="page-item"><Link className="page-link" to={`?page=${currentPage + (neighbours*2) + 1}&offset=${offset}`} >&#8680;</Link></li>
+                            }
+                            return <li key={index} className="page-item"><Link className={`page-link${ parseInt(currentPage) === page ? ' active' : ''}`} to={`?page=${page}&offset=${offset}`}>{page}</Link></li>
+                        })}
+                    </ul>)}
                 </nav>
+                {/*<div className="input-field">*/}
+                {/*    <label htmlFor="selectPage">Select Page: </label>*/}
+                {/*    <input id="selectPage" onChange={e => dispatch(setCurrentPage(e.target.value))} type="number" placeholder="Select page..." max={totalPages} value={currentPage}/>*/}
+                {/*</div>*/}
             </div>  }
         </div>
     );
 }
-
+const TablePaginator = styled(TablePaginatorRaw)`
+  .nav-wrapper {
+    display:flex;
+    align-items: center;
+    margin-bottom:30px;
+  }
+  .input-field {
+    margin-left:20px;
+    width:200px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    input {
+      width:100px;
+      padding:5px;
+    }
+  }
+  ul {
+    margin-bottom:0
+  }
+  li {
+    outline: none;
+    box-shadow: inset 0 -1px 0 #ddd;
+  }
+  a {
+    outline:none;
+    box-shadow: inset 0 -1px 0 #ddd;
+    &:focus {
+      outline:none;
+      box-shadow: inset 0 -1px 0 #ddd;
+    }
+    &.active {
+      background: blue;
+      color:white;
+    }
+  }
+`
 export default TablePaginator;
--- src/components/tablePaginator/tablePaginatorSlice.js
+++ src/components/tablePaginator/tablePaginatorSlice.js
@@ -1,11 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { createSelector } from "@reduxjs/toolkit";
+import { makeArray} from "../../utils";
 
 export const tablePaginatorSlice = createSlice({
     name: 'paginator',
     initialState: {
         offset: 25,
         totalPages: 0,
-        currentPage: 1
+        currentPage: 1,
+        neighbours: 2
     },
     reducers: {
         setOffset: (state, action) => {
@@ -16,10 +19,13 @@ export const tablePaginatorSlice = createSlice({
             state.offset = action.payload
         },
         setTotalPages: (state, action) => {
-            state.totalPages = action.payload
+            state.totalPages = parseInt(action.payload)
         },
         setCurrentPage: (state, action) => {
-            state.currentPage = action.payload
+            state.currentPage = parseInt(action.payload)
+        },
+        setNeighbours : (state, action) => {
+            state.neighbours = parseInt(action.payload)
         }
     },
 });
@@ -28,5 +34,40 @@ export const { setOffset, setTotalPages, setCurrentPage } = tablePaginatorSlice.
 export const selectOffset = state => state.paginator.offset;
 export const selectTotalPages = state => state.paginator.totalPages
 export const selectCurrentPage = state => state.paginator.currentPage
+export const selectNeighbours = state => state.paginator.neighbours
+
+export const selectPaginationList = createSelector(
+    selectTotalPages,
+    selectCurrentPage,
+    selectNeighbours,
+    (totalPages, currentPage, neighbours) =>  {
+        console.log("TOTAL PAGES: ",totalPages)
+        const pagesWithoutNeighbours = (neighbours * 2) + 3
+        const pagesWithNeighbours = pagesWithoutNeighbours + 2
+
+        if(totalPages > pagesWithNeighbours) {
+            const startPage = Math.max(2, currentPage - neighbours)
+            const endPage = Math.min(totalPages -1, currentPage + neighbours)
+            let pages = makeArray(startPage, endPage)
+
+            const hasLeftSpill = startPage > 2;
+            const hasRightSpill = (totalPages - endPage) > 1;
+            const spillOffset = pagesWithoutNeighbours - (pages.length + 1)
+
+            if(hasLeftSpill && !hasRightSpill) {
+                const extraPages = makeArray(startPage - spillOffset, startPage - 1);
+                pages = ["PREV", ...extraPages, ...pages];
+            }else if(!hasLeftSpill && hasRightSpill) {
+                const extraPages = makeArray(endPage + 1, endPage + spillOffset);
+                pages = [...pages, ...extraPages, "NEXT"];
+            }else if(hasLeftSpill && hasRightSpill) {
+                pages = ["PREV", ...pages, "NEXT"];
+            }
+
+            return [1, ...pages, totalPages]
+        }
+        return makeArray(1, totalPages)
+    }
+)
 
 export default tablePaginatorSlice.reducer
